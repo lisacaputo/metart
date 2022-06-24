252870cf56c71992882d7d0e0722034fa1a66727
Lisa Caputo <lcaputo7788@yahoo.com>
2022-06-24 17:11:59 -0400

    added a few more data fields to capture

--- script.js
+++ script.js
@@ -10,13 +10,26 @@ const getObjectID = async () => {
         `
     )
 
-    //json - javascript object notation, we know we can use dot notation to access objects
+    //*** Artist Info ***/
+    let artistName = response.data.artistDisplayName
+    let artistNationality = response.data.artistNationality
 
+    //*** Artwork Info ***/
     let title = response.data.title
     let image = response.data.primaryImage
-    contentDiv.innerHTML = `<span>${title}</span><br /><img src=${image} alt="artwork" />`
+    //Year, a span of years, or a phrase that describes the specific or approximate date when an artwork was designed or created
+    let objectDate = response.data.objectDate
+    //Refers to the materials that were used to create the artwork
+    let medium = response.data.medium
+    //location of artwork in the Met
+    let gallery = response.data.GalleryNumber
+    
+    
+    contentDiv.innerHTML =`<span>${title}</span><br /><img src=${image} alt="artwork" />`
  
     console.log(response)
 }
 
 button.addEventListener('click', getObjectID)
+
+//Keep using 459201 as the objectID for testing purposes
